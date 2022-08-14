204b0fcccba20710b79dda5c6c16bcb1f952751d
happiguru <hguruman@gmail.com>
2022-08-15 01:09:08 +0400

    Add post scaffold

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :post do
+    title { "MyString" }
+    body { "MyText" }
+  end
+
   factory(:user) do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
